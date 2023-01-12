ac4b0123634338d6dfc989c0e00672b9618c039a
Sean Wong <sean.uuong@gmail.com>
2023-01-11 21:03:55 -0800

    Complete bare_minimum/callbackReview.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, dataHandler) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      dataHandler(err, null);
+    } else {
+      var line = data.slice(0, data.indexOf('\n'));
+      dataHandler(null, line);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, urlHandler) {
+  request.get(url, (err, response, body) => {
+    if (err) {
+      urlHandler(err, null);
+      console.log('ERROR!', err);
+    } else {
+      urlHandler(null, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
